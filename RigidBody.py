baef83ca52be0cc95c12426e3c4e7414389f3db3
Olga <olga.petrova@cvut.cz>
2017-07-22 20:13:08 +0200

    Update RigidBody class.

--- RigidBody.py
+++ RigidBody.py
@@ -4,16 +4,13 @@ from pygame.locals import *
 from Rect import *
 
 
-def rot(a):
-    return np.array([[np.cos(a), -np.sin(a)], [np.sin(a), np.cos(a)]])
-
-
-class Rigid_body:
+class RigidBody:
     def __init__(self, **kwargs):
 
         # graphical
-        self.width = kwargs.get('width', 6)
-        self.length = kwargs.get('length', 15)
+        self.id = kwargs.get('id', 0)
+        self.width = kwargs.get('width', 10)
+        self.length = kwargs.get('length', 20)
         self.color = kwargs.get('color', (0, 0, 255))
         self.body = Rect(kwargs.get('x', 0.0), kwargs.get('y', 0.0), self.width, self.length, kwargs.get('angle', 0))
 
@@ -27,6 +24,14 @@ class Rigid_body:
         self.m_torque = 0.0
         self.m_inertia = (1.0 / 12.0) * 6**2 * 15**2 * self.m_mass
 
+    @property
+    def x(self):
+        return self.m_pos[0][0]
+
+    @property
+    def y(self):
+        return self.m_pos[1][0]
+
     def update(self, timestep):
         acc = self.m_forces / self.m_mass
         self.m_vel += acc * timestep
@@ -34,11 +39,12 @@ class Rigid_body:
         # print('Forces:', self.m_forces[0], self.m_forces[1], end='')
         # print('Velocity:', self.m_vel)
 
-
         ang_acc = self.m_torque / self.m_mass
         self.m_ang_vel += ang_acc * timestep
         self.m_angle += self.m_ang_vel * timestep
         # print(' Torque:', self.m_torque)
+        self.m_torque = 0
+        self.m_forces = np.zeros_like(self.m_forces)
 
     def point_vel(self, w_off):
         tangent = np.array([-w_off[1], w_off[0]])
@@ -56,25 +62,22 @@ class Rigid_body:
         trans = rot(self.m_angle)
         return trans.dot(rel)
 
-
-    def draw(self, DISPLAY, font=None, x=0, y=0):
+    def draw(self, display, font=None, x=0, y=0):
         self.body.rotate(self.m_angle)
-        self.body.translate(self.m_pos[0][0], self.m_pos[1][0])
-        pygame.draw.polygon(DISPLAY, self.color,
-                            self.body.spirit(-x + DISPLAY.get_width() / 2, -y + DISPLAY.get_height() / 2))
-        if font:
-            text = str('Force: [%.2f, %.2f]'%(self.m_forces[0], self.m_forces[1]))
-            text2 = str('Torque: %.2f,' % self.m_torque)
-            text = font.render(text, True, (0,0,0))
-            DISPLAY.blit(text, (0, 0))
-            text2 = font.render(text2, True, (0, 0, 0))
-            DISPLAY.blit(text2, (0, 15))
-        self.m_torque = 0
-        self.m_forces = np.zeros_like(self.m_forces)
-        # pygame.draw.line(DISPLAY, (0, 255, 0), (self.m_pos[0] + w_off[0],
-        #                                         self.m_pos[1] + w_off[1]),
-        #                  (self.m_pos[0] + w_off[0] - 1000 * self.m_torque * w_off[1] / np.linalg.norm(w_off),
-        #                   self.m_pos[1] + w_off[1] + 1000 * self.m_torque * w_off[0] / np.linalg.norm(w_off)))
+        self.body.translate(self.x, self.y)
+        pygame.draw.polygon(display, self.color,
+                            self.body.spirit(-x + display.get_width() / 2, -y + display.get_height() / 2))
+        # if self.id == 0:
+        #     text = str('Force: [%.2f, %.2f]' % (self.m_forces[0], self.m_forces[1]))
+        #     text2 = str('Torque: %.2f,' % self.m_torque)
+        #     text = font.render(text, True, (0, 0, 0))
+        #     display.blit(text, (0, 0))
+        #     text2 = font.render(text2, True, (0, 0, 0))
+        #     display.blit(text2, (0, 15))
 
+        text = font.render(str(self.id), True, (0, 0, 0))
+        display.blit(text, (
+        self.x - x + display.get_width() / 2 - self.width, self.y - y + display.get_height() / 2 - self.length))
 
-
+        # self.m_torque = 0
+        # self.m_forces = np.zeros_like(self.m_forces)
